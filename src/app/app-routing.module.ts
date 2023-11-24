1a2787294f82b20c6e5481b34087278d3fddf588
marion-sgr <marion.segria@gmail.com>
2023-11-24 14:18:51 +0100

    Add components and set router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProfilComponent } from './components/profil/profil.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { InscriptionComponent } from './components/inscription/inscription.component';
+
+const routes: Routes = [
+  {
+    path: 'profil',
+    component: ProfilComponent
+  },
+  {
+    path: 'connexion',
+    component: ConnexionComponent
+  },
+  {
+    path: 'inscription',
+    component: InscriptionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
