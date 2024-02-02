89951b64c68de0dfc779cf3803426665ee71e515
marion-sgr <marion.segria@gmail.com>
2024-02-02 11:37:14 +0100

    add environment

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -10,7 +10,8 @@ export class AppService {
   getbyId(id: number) {
     throw new Error('Method not implemented.');
   }
-  api = 'http://localhost:3000/v1';
+  // api = 'http://localhost:3000/v1';
+  api = 'https://potits-chats-back.vercel.app/v1';
 
   constructor(private http: HttpClient) {}
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  auth: {
+    domain: 'dev-potitschat.eu.auth0.com',
+    clientId: 'eTJncrvP8o5RyOMhuUlXod6OG4Iqgl2h',
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
+  },
+};
