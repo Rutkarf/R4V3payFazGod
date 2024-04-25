3b695b399589c97e949589c187f9bce86553cd16
marion-sgr <marion.segria@gmail.com>
2024-04-25 13:56:44 +0200

    add primeng and test it with upload file component

--- src/app/components/associations/associations-list/associations-list.component.ts
+++ src/app/components/associations/associations-list/associations-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { FileUploadEvent } from 'primeng/fileupload';
 
 @Component({
   selector: 'app-associations-list',
   templateUrl: './associations-list.component.html',
-  styleUrls: ['./associations-list.component.scss']
+  styleUrls: ['./associations-list.component.scss'],
+  providers: [MessageService]
 })
 export class AssociationsListComponent {
+  uploadedFiles: any[] = [];
 
+  constructor(private messageService: MessageService) {}
+
+  onUpload(event:FileUploadEvent) {
+      for(let file of event.files) {
+          this.uploadedFiles.push(file);
+      }
+
+      this.messageService.add({severity: 'success', summary: 'Fichier importé', detail: ''});
+  }
 }
